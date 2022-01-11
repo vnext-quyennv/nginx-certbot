d78b16fe94dd7aedabf0ca8f440ac0a7aa06089e
quyennv <quyennv@vnext.com.vn>
2022-01-11 17:47:40 +0700

    fix: Test for new domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(test.takayama.tv www.test.takayama.tv)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
