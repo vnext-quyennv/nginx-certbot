a5ad547ff36748fc0eed95df6c9a477d1d3b8a0b
quyennv <quyennv@vnext.com.vn>
2022-01-11 19:27:39 +0700

    fix: Add new domain for SSL

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(test.takayama.tv www.test.takayama.tv)
+domains=(test.takayama360.tv www.test.takayama360.tv)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
